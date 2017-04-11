f1351c14f39cc95819e105b1610f5ec9d750a8b8
Mehdi Massoudi <mehdi@bitbrew.com>
2017-04-11 10:32:53 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 INSTALL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+command -v npm >/dev/null 2>&1 || { echo >&2 "I require npm, but it's not installed.  Aborting."; exit 1; }
+
 mkdir -p "$INSTALL_DIR/node_modules"
 npm install --prefix $INSTALL_DIR $INSTALL_DIR
 ln -fs "$INSTALL_DIR/consumer" /usr/local/bin/consumer
